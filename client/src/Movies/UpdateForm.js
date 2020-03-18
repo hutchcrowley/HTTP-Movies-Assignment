e72090dfeac7a2d964ac3dd90e46781f9b148c0a
James Hargis <okcomputer@go-fuck-yourself.local>
2020-03-17 21:00:59 -0500

    Update - add UpdateForm

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -8,15 +8,15 @@ const UpdateForm = props => {
 
   console.log("Props in update form: ", props);
 
-  const id = Number(props.match.params.id);
+  const updateId = Number(props.match.params.id);
 
-  console.log("ID: ", id);
+  console.log("ID: ", updateId);
 
   const [newMovie, setNewMovie] = useState();
 
-  const updateMovie = id => {
+  const updateMovie = updateId => {
     axios
-      .put(`http://localhost:5000/movies/${id}`, newMovie)
+      .put(`http://localhost:5000/movies/${updateId}`, newMovie)
       .then(res => {
         console.log("Request returned from API PUT request: ", res.data);
         setNewMovie(res.data);
@@ -24,7 +24,7 @@ const UpdateForm = props => {
       .catch(err => {
         console.log("ERROR: data not sent to API via PUT: ", err);
       });
-  };``
+  };
 
   const handleChange = e => {
     setNewMovie({
@@ -36,5 +36,56 @@ const UpdateForm = props => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    updateMovie(newMovie);
+    history.push("/");
   };
+
+  return (
+    <div classNme="update-form">
+      <h1>Edit Movie</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <input
+            className="title-input"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            placeholder="Title"
+          />
+        </label>
+        <label htmlFor="director">
+          <input
+            className="director-input"
+            type="text"
+            name="director"
+            onChange={handleChange}
+            placeholder="Director"
+          />
+        </label>
+        <label htmlFor="metascore">
+          <input
+            className="metascore-input"
+            type="text"
+            name="metascore"
+            onChange={handleChange}
+            placeholder="Metascore"
+          />
+        </label>
+        <label htmlFor="stars">
+          <input
+            className="stars-input"
+            type="text"
+            name="stars"
+            onChange={handleChange}
+            placeholder="stars"
+          />
+        </label>
+
+        <input className="button update-button" type="submit" />
+      </form>
+    </div>
+  );
 };
+
+export default UpdateForm;
